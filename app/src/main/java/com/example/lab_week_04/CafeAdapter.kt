7cd76cbb12b5cfa022b2acc735bfa55391aed4ed
henrysalim <henrysalim22@gmail.com>
2025-09-19 15:17:27 +0700

    Commit No. 4 - Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.lab_week_04
 
+import android.os.Bundle
+import androidx.core.content.ContextCompat.getString
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -11,12 +14,36 @@ val TABS_FIXED = listOf(
     R.string.kopikenangan_title
 )
 
-class CafeAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fragmentManager, lifecycle) {
+class CafeAdapter(private val fragment: Fragment) :
+    FragmentStateAdapter(fragment) {
+    private var TAB_CONTENT = "TAB_CONTENT"
+
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return CafeDetailFragment()
+        return when (position) {
+            0 -> CafeDetailFragment().apply {
+                arguments = bundleOf(
+                    "TAB_CONTENT" to fragment.getString(R.string.starbucks_desc),
+                )
+            }
+
+            1 -> CafeDetailFragment().apply {
+                arguments = bundleOf(
+                    "TAB_CONTENT" to fragment.getString(R.string.janjijiwa_desc),
+                )
+            }
+
+
+            2 -> CafeDetailFragment().apply {
+                arguments = bundleOf(
+                    "TAB_CONTENT" to fragment.getString(R.string.kopikenangan_desc),
+                )
+            }
+
+            else -> throw IllegalStateException("Invalid position $position")
+        }
     }
 }
